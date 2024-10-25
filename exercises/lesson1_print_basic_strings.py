08b55ecb7d9633e116c779bc419fdead6c85a17b
The Emeryverse <server@emeryverse.com>
2024-10-24 20:47:59 -0500

    1 lesson

--- exercises/lesson1_print_basic_strings.py
+++ exercises/lesson1_print_basic_strings.py
@@ -1,12 +1,13 @@
 # 1. Print a short story using multiple lines.
 # TODO: 
-
+print("There was a boy named Greyson.\nHe loved salt, Taylor Swift, and Minecraft.\nHe was a very good boy.")
 # 2. Create a string that includes both single and double quotes.
 # TODO:
-
+print("\"Hi!\" Greyson said. \n\"Can you say the \'n-word\'?\"\n\"Yes. Yes I most certainly can.\"")
 # 3. Use escape characters to create a formatted list.
 # TODO:
-
+print("My favorite foods:\n\t- Mac n cheese\n\t- Pizza")
 # 4. Combine different types of data (strings and numbers) in one print statement.
 # TODO:
-
+print("My name is Hudson and I am",25,"years old!")
+print("tab 1\t tab 2\t tab 3")
