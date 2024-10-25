0d004fc3ade8f47fc73d298836b4ba0755cea309
TheEmeryverse <server@emeryverse.com>
2024-10-24 20:20:28 -0500

    1

--- exercises/lesson1_multi_line_printing.py
+++ exercises/lesson1_multi_line_printing.py
@@ -1,11 +1,17 @@
 # Try printing a simple three-line text or poem using three separate print() statements.
 #TODO:
+print("This is line 1.")
+print("This is line 2. ")
+print("This is line 3.")
 
 # Try printing a simple three-line text or poem using one print() statement with \n.
 #TODO:
+print("This is line 1.\nThis is line 2.\nThis is line 3.")
 
 # Print it using triple quotes.
 #TODO:
+print("""This is line 1.\nThis is line 2.\nThis is line 3.""")
 
 # Add a blanc line between each sting/verse of your text/poem using any method you prefer.
 #TODO:
+print("This is line 1.\n\nThis is line 2.\n\nThis is line 3.")
